f62102d54135edf55760658dbc29997b8ad1c6bd
Hazel Baker-Harvey <84642569+hazelbakerharvey@users.noreply.github.com>
2021-09-05 19:17:51 -0700

    oops i fixed a thing

--- src/test/java/com/ss/eastcoderbank/router/RouterApplicationTests.java
+++ src/test/java/com/ss/eastcoderbank/router/RouterApplicationTests.java
@@ -1,13 +1,8 @@
 package com.ss.eastcoderbank.router;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RouterApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
